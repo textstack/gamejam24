448f3dc07f1f5588f5c8b6569b979456b3a65fb8
textstack <textstack@protonmail.com>
2024-11-01 18:54:31 -0700

    currency system and health

---
+++ global/currency.gd
@@ -0,0 +1,34 @@
+extends Node
+
+class_name Currency
+
+
+var icon = ""
+var currencyName = "Money"
+var total = 0
+
+
+func _init(startTotal, currencyName, icon):
+	total = startTotal
+	currencyName = currencyName
+	icon = icon
+
+
+func getName():
+	return currencyName
+
+
+func getIcon():
+	return icon
+
+
+func add(amount):
+	total += amount
+
+
+func trySpend(amount):
+	if total < amount:
+		return false
+	
+	total -= amount
+	return true
